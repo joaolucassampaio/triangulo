499d2e075be5c1621849ca2d16231b4663d803ab
joaolucassampaio <160886470+joaolucassampaio@users.noreply.github.com>
2024-04-03 19:34:50 -0300

    Add files via upload

---
+++ Diagnostica2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("    #\n");
+    printf("   ###\n");
+    printf("  #####\n");
+    
+    int base = 5;
+    int altura = 3;
+    float area = (base * altura) / 2.0;
+    printf("Aarea do tria¢ngulo: %f\n", area);
+
+    return 0;
+}
